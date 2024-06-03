9f910346c09a56522e7e06b1f3208c8e5190aa71
Nisarg Panchal <nisarg.panchal@justice.qld.gov.au>
2024-06-03 11:34:42 +1000

    fixed SBA and WIP for elastic search

--- src/main/java/com/nisarg/dockerdemo/controller/PeopleController.java
+++ src/main/java/com/nisarg/dockerdemo/controller/PeopleController.java
@@ -1,7 +1,9 @@
 package com.nisarg.dockerdemo.controller;
 
 import com.nisarg.dockerdemo.bean.Person;
+import com.nisarg.dockerdemo.elasticsearch.repository.ElPeopleRepository;
 import com.nisarg.dockerdemo.repository.PeopleRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,18 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/people")
 public class PeopleController {
 
     private final PeopleRepository peopleRepository;
-
-    public PeopleController(PeopleRepository peopleRepository) {
-        this.peopleRepository = peopleRepository;
-    }
+    private final ElPeopleRepository elPeopleRepository;
 
     @GetMapping("{personId}")
     public Mono<Person> getPersonById(@PathVariable UUID personId) {
@@ -32,6 +36,15 @@ public class PeopleController {
     @GetMapping("/")
     public Flux<Person> findAll() {
         log.info("Find All");
-        return peopleRepository.findAll();
+        List<Person> people = new LinkedList<>();
+        Flux<Person> peopleFlux = peopleRepository.findAll();
+        peopleFlux.subscribe(people::add);
+        if (!people.isEmpty()) {
+            log.info("Storing {} people in Elastic-search", people.size());
+            elPeopleRepository.saveAll(people);
+        } else {
+            log.info("No people stored in Elastic-search");
+        }
+        return peopleFlux;
     }
 }
--- src/main/java/com/nisarg/dockerdemo/repository/PeopleRepository.java
+++ src/main/java/com/nisarg/dockerdemo/repository/PeopleRepository.java
@@ -2,8 +2,9 @@ package com.nisarg.dockerdemo.repository;
 
 import com.nisarg.dockerdemo.bean.Person;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 
 import java.util.UUID;
 
-public interface PeopleRepository extends ReactiveCrudRepository<Person, UUID> {
+public interface PeopleRepository extends ReactiveSortingRepository<Person, UUID>, ReactiveCrudRepository<Person, UUID> {
 }
